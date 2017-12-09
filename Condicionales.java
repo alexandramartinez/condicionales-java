713c7f46deeabb2df820350dbe4912204358e462
Alexandra Martinez <nahomibj@gmail.com>
2017-12-09 16:28:01 -0600

    First commit

---
+++ Condicionales.java
@@ -0,0 +1,11 @@
+public class Condicionales {
+    public static void main(String args[]) {
+        int huevos = 0;
+	
+	if(huevos > 0) {
+	    System.out.println("Cocinar");
+	} else {
+	    System.out.println("Ir a comprar huevos");
+	}
+    }
+}
